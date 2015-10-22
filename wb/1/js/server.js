587ea8eee3281800e94aadc9933394b4215c0774
wangrun <yeesunday@gmail.com>
2015-10-22 16:52:32 +0800

    x

--- wb/1/js/server.js
+++ wb/1/js/server.js
@@ -41,8 +41,9 @@ function getDistance(callback) {
     ldl.getUserDataInapp().done(function() {
       //获取当天运动数据
       ldl.app.getDailyStatsWithData(data).done(function(d) {
-        //alert('1 ' +  d);
-        //alert('2' + JSON.parse(d))
+        alert('1 ' +  d);
+        alert('2' + JSON.parse(d));
+        alert('3' + JSON.parse(JSON.parse(d)));
         callback(JSON.stringify(JSON.parse(JSON.parse(d))));
       });
     });
